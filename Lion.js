48fc1ed18891743ea951a6fc1ae3f5e7422044c4
OorjitVedamurthy123 <63858193+OorjitVedamurthy123@users.noreply.github.com>
2020-05-15 23:27:22 +0530

    Added different backgrounds

---
+++ Lion.js
@@ -0,0 +1,17 @@
+class Lion extends BaseClass {
+  constructor(x,y){
+    
+    super(x,y,90,90);
+    this.image = loadImage("lion.png");
+    this.smoke = loadImage("smoke.png");
+    
+  }
+
+  display() {
+   
+    super.display();
+  
+
+    
+  }
+}
